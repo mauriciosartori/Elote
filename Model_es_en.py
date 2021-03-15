d12d5965a8e57c7f1c8f2cafcb6702d48b2dd927
a1260606 <mau.guerrero@bestbuy.com>
2021-03-14 22:43:58 -0700

    Add missed translator capabilities

--- Model_es_en.py
+++ Model_es_en.py
@@ -3,7 +3,7 @@ from transformers import MarianMTModel, MarianTokenizer
 
 class Model_es_en:
 
-    def get_list_sentences_es(self, corpora_name):
+    def get_list_sentences(self, corpora_name):
         with open(corpora_name) as file:
             full_base_data_set = file.readlines()
 
@@ -21,23 +21,31 @@ class Model_es_en:
             list_es_sentences.append(formatted_sentence)
         return list_es_sentences
 
-    def tranlate_sentences_es_to_engl(self):
-        list_src_es_sentences = self.get_list_sentences_es('resources/base_tedx_spanish_corpora.txt')
-        print(list_src_es_sentences)
+    def translate_sentences_es_to_en(self):
+        print("Translating sentences from Spanish to English...")
+        list_src_es_sentences = self.get_list_sentences('resources/base_tedx_spanish_corpora.txt')
 
-        # model_name = 'Helsinki-NLP/opus-mt-en-es'
         model_name = 'Helsinki-NLP/opus-mt-es-en'
-        tokenizer = MarianTokenizer.from_pretrained(model_name)
-        print(tokenizer.supported_language_codes)
-        model = MarianMTModel.from_pretrained(model_name)
-        translated = model.generate(**tokenizer.prepare_seq2seq_batch(list_src_es_sentences, return_tensors="pt"))
-        list_trans_sentences_es_en = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
-        print("Results:")
-        print(list_trans_sentences_es_en)
+        list_trans_sentences_es_en = self.tranlate_from_MarianModel(model_name)
         self.write_en_sentences(list_trans_sentences_es_en)
         print("Translation done")
 
-    def write_en_sentences(self,list_es_sentences):
+    def write_en_sentences(self, list_es_sentences):
         with open('resources/generated_tedx_english_corpora.txt', 'w') as file:
-            file.write(str(list_es_sentences))
+            file.write("\n".join(list_es_sentences))
 
+    def translate_sentences_en_to_es(self):
+        print("Translating sentences from English to Spanish...")
+        list_src_en_sentences = self.get_list_sentences('resources/generated_tedx_english_corpora.txt')
+
+        model_name = 'Helsinki-NLP/opus-mt-en-es'
+        list_trans_sentences_es_en = self.tranlate_from_MarianModel(model_name)
+        self.write_en_sentences(list_trans_sentences_es_en)
+        print("Translation done")
+
+    def tranlate_from_MarianModel(self, model_name):
+        tokenizer = MarianTokenizer.from_pretrained(model_name)
+        # print(tokenizer.supported_language_codes)
+        model = MarianMTModel.from_pretrained(model_name)
+        translated = model.generate(**tokenizer.prepare_seq2seq_batch(list_src_es_sentences, return_tensors="pt"))
+        return [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from Model_es_en import Model_es_en
 
 if __name__ == '__main__':
     model_es_en = Model_es_en()
-    model_es_en.tranlate_sentences_es_to_engl()
+    model_es_en.translate_sentences_es_to_en()
