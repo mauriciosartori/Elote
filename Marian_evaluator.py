b1ac45004027fe8fbbbf01e18d43ad125a21f886
a1260606 <mau.guerrero@bestbuy.com>
2021-03-17 20:57:09 -0700

    Changing approach; add Twitter corpus

--- Marian_evaluator.py
+++ Marian_evaluator.py
@@ -1,3 +1,5 @@
+import json
+
 from transformers import MarianMTModel, MarianTokenizer
 
 
@@ -15,7 +17,7 @@ def translate_from_MarianModel(model_name, list_src_es_sentences):
 
 
 class Marian_evaluator:
-    number_of_sentence_to_evaluate = 50
+    number_of_sentence_to_evaluate = 1500
 
     def get_list_sentences(self, corpora_name):
         with open(corpora_name) as file:
@@ -34,14 +36,38 @@ class Marian_evaluator:
             list_es_sentences.append(formatted_sentence)
         return list_es_sentences
 
-    def translate_sentences_es_to_en(self):
-        print("Translating sentences from Spanish to English...")
-        list_src_es_sentences = self.get_list_sentences('resources/base_tedx_spanish_corpora.txt')
+    def get_list_candidate_items_from_train(self):
+        with open('resources/twt.train.json') as file:
+            full_base_data_set = file.readlines()
 
-        model_name = 'Helsinki-NLP/opus-mt-es-en'
-        list_trans_sentences_es_en = translate_from_MarianModel(model_name, list_src_es_sentences)
-        write_sentences_in_file(list_trans_sentences_es_en, 'resources/generated_tedx_english_corpora.txt')
-        print("Translation done")
+        # We can control the number of sentences to work with
+        number_sentences_trained = 0
+        list_candidate_tokens = []
+
+        for sentence_split_str in full_base_data_set:
+            number_sentences_trained += 1
+            if number_sentences_trained == self.number_of_sentence_to_evaluate:
+                break
+            sentence_split_json = json.loads(sentence_split_str)
+            sentence = ""
+            save_sentence = False
+            for index in range(3, len(sentence_split_json), +1):
+                single_word_compose = sentence_split_json[index]
+                single_word = single_word_compose[0]
+                if single_word.lower() == "the":
+                    save_sentence = True
+                sentence += " " + single_word
+            if save_sentence:
+                list_candidate_tokens.append(sentence)
+
+        return list_candidate_tokens
+
+    def generate_sentences_to_analyze(self):
+        print("Translating sentences from Spanish to English...")
+        list_src_es_sentences = self.get_list_candidate_items_from_train()
+        print("Number of sentence found:" + str(len(list_src_es_sentences)))
+        write_sentences_in_file(list_src_es_sentences, 'resources/candidate_sentences.txt')
+        print("Sentences generated and saved")
 
     def translate_sentences_en_to_es(self):
         print("Translating sentences from English to Spanish...")
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Marian_evaluator import Marian_evaluator
 
 if __name__ == '__main__':
     model_es_en = Marian_evaluator()
-    #model_es_en.translate_sentences_es_to_en()
+    model_es_en.generate_sentences_to_analyze()
     #model_es_en.translate_sentences_en_to_es()
-    model_es_en.evaluate_results()
+    #model_es_en.evaluate_results()
